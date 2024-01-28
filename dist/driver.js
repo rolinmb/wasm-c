e0a065022f76181859ee29958e0a8dd2402b951c
Rolin Blake <rolinmblake@gmail.com>
2024-01-28 17:34:38 -0600

    memory free and alloc in wasm from c (video 4)

--- dist/driver.js
+++ dist/driver.js
@@ -37,34 +37,69 @@ var memory = new WebAssembly.Memory({
     initial: 256,
     maximum: 512
 });
+
 var exports;
 WebAssembly.instantiateStreaming(
     fetch("main.wasm"), {
     js: {
         mem: memory
+    },
+    env: {
+        emscripten_resize_heap: function(delta) {
+            memory.grow(delta);
+        }
     }
 }).then(results => {
     exports = results.instance.exports;
     memory = results.instance.exports.memory;
 });
+
 function run_wasm() {
-    var arr = new Uint32Array(memory.buffer);
-    for (var i = 0; i < 10; i++) {
-        arr[i] = i * 2;
-    }
-    var sum = exports.accumulate(arr, 10);
+    var arr = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20];
+    var ptr = encode_array(arr, arr.length, 4);
+    var sum = exports.accumulate(ptr,arr.length);
+    exports.wasm_free(ptr);
     document.getElementById("val-out").innerHTML = sum; 
 }
+
 function get_string() {
-    var ptr = exports.getString();
+    var str = decode_string(exports.getString());
+    alert("'"+str+"' has been copied to clipboard");
+    navigator.clipboard.writeText(str);
+}
+
+function encode_array(arr, len, sizeof = 1) {
+    var ptr;
+    var out;
+    if (sizeof == 8) {
+        ptr = exports.wasm_malloc(len * 8);
+        out = new BigUint64Array(memory.buffer, ptr);
+    } else if (sizeof == 4) {
+        ptr = exports.wasm_malloc(len * 4);
+        out = new Uint32Array(memory.buffer, ptr);
+    } else {
+        ptr = exports.wasm_malloc(len);
+        out = new Uint8Array(memory.buffer, ptr);
+    }
+    for (var i = 0; i < len; i++) {
+        out[i] = arr[i];
+    }
+    return ptr;
+}
+
+function decode_array(ptr, len) {
+    return new Uint8Array(memory.buffer).slice(ptr, ptr + len);
+}
+
+function decode_string(ptr, len) {
+    return new TextDecoder("utf8").decode(decode_array(ptr, len));
+}
+
+function decode_string(ptr) {
     var bytes = new Uint8Array(memory.buffer, ptr);
     var strlen = 0;
     while (bytes[strlen] != 0) {
-        strlen ++;
+        strlen++;
     }
-    var str = new TextDecoder("utf8").decode(
-        bytes.slice(0, strlen)
-    );
-    alert("'"+str+"' has been copied to clipboard");
-    navigator.clipboard.writeText(str);
+    return new TextDecoder("utf8").decode(bytes.slice(0, strlen));
 }
--- src/c/memory.c
+++ src/c/memory.c
@@ -1,4 +1,5 @@
 #include <emscripten.h>
+#include <stdlib.h>
 
 int main() { return 0; }
 
@@ -14,4 +15,14 @@ int accumulate(int *arr, int n) {
 EMSCRIPTEN_KEEPALIVE
 const char *getString() {
   return "Hello wasm, this is C from emcc!";
+}
+
+EMSCRIPTEN_KEEPALIVE
+void *wasm_malloc(size_t n) {
+  return malloc(n);
+}
+
+EMSCRIPTEN_KEEPALIVE
+void wasm_free(void *ptr) {
+  return free(ptr);
 }
